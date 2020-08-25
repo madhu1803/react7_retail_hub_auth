ae4ec28b6bae21d1e243d4310ed6987049f0056c
madhumithaa <madhumithaa1411@gmail.com>
2020-08-25 21:02:59 +0530

    done

--- src/Components/RightContainer.js
+++ src/Components/RightContainer.js
@@ -4,6 +4,20 @@ import { AiFillCodeSandboxCircle } from "react-icons/ai";
 import Input from "./Input";
 import { Button } from "react-bootstrap";
 export default class RightContainer extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  submitHandler = () => {
+    alert("Submitted");
+    console.log(this.state.email);
+    console.log(this.state.password);
+    this.setState({ email: "", password: "" });
+  };
   render() {
     return (
       <div className="container right-container text-capitalize">
@@ -14,10 +28,26 @@ export default class RightContainer extends Component {
           </h4>
           <p className="text-muted pt-3 mt-5">please login to your account</p>
         </div>
-        <Input type="email" placeholder="Email Address" />
-        <Input type="password" placeholder="Password" />
+        <Input
+          type="email"
+          placeholder="Email Address"
+          value={this.state.email}
+          name="email"
+          change={(e) => this.handleChange(e)}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          value={this.state.password}
+          name="password"
+          change={(e) => this.handleChange(e)}
+        />
         <p className="forgot-psd">Forgot Password?</p>
-        <Button variant="success" className="login-btn mt-5">
+        <Button
+          variant="success"
+          className="login-btn mt-5"
+          onClick={this.submitHandler}
+        >
           Login
         </Button>
         <p className="text-muted txt-container">
